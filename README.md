a2120a022cb2431fbfccda36fb8f05570747c24c
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2021-07-04 11:59:41 +0200

    canvas and canvasDiv set, Keyword class and draw method declared and tested

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # MIND-MAP-APP
- Simple and intuitive app for mind mapping using p5.js library
+ Simple and intuitive app for mind mapping using p5.js library.
+
+I love mindmapping and use it everyday.
+Heard of it? If not, you really should to know mind maps better!
+This is very powerfull tool for your mind.
+You can use it while studying, learning, creating, planning, reading etc.
+
+The basic idea of mind maps is to distract keywords from the information you are reading or listening and put them into mind map.
+
+In the middle of your mindmap the core keyword should be.
+It can be not only one word. That can be a topic, a full sentence etc.
+
+You put keywords around the Core Keyword and connect them with the core with lines.
+
+Every Keyword can initiate a new branch/ topic and become a Core Keyword for other keyword, which will be related with this particular keyword.
+
+So you see, that you are building a structure of related keywords.
+
+Usually I do mindmapping on a paper and prefer this method.
+I've tried a few apps for mindmapping, but wasn't satisfied...
+So I decided to build my own app :-)
+
+Also this is a good opportunity for me to get back to (and recall) p5.js library.
+
+I hope you'll enjoy it!
+
+PS. if you want to save your mind map, just press mouse right button and Save an image. Thats' all! Now you can share it, put it in your presentation or print.
--- sketch.js
+++ sketch.js
@@ -1,9 +1,44 @@
+let canvasWidth, canvasHeight;
+
+class Keyword {
+    constructor(index, x, y, text, color) {
+        this.index = index;
+        this.text = text;
+        this.x = x;
+        this.y = y;
+        this.w = this.text.length*9; // width depends on text length
+        this.h = 30;
+        this.color = color;
+    }
+    draw() {
+        // shape (currently rect with rounded corners)
+        fill(this.color);
+        rectMode(CENTER);
+        rect(this.x, this.y, this.w, this.h, 2);
+        // text
+        fill("black");
+        textAlign("center", "center");
+        textSize(15);
+        text(this.text, this.x, this.y);
+    }
+}
+
 function setup() {
-  createCanvas(400, 400);
-  background(250);
-  
+    canvasWidth = 600;
+    canvasHeight = 400;
+    // putting canvas into the div
+    const canvas = createCanvas(canvasWidth, canvasHeight);
+    canvas.parent("canvas");
+    // auto resizing canvasDiv to canvas width and height
+    const canvasDiv = document.getElementById("canvas");
+    canvasDiv.style.width = canvasWidth + "px";
+    canvasDiv.style.height = canvasHeight + "px";
 }
 
 function draw() {
-  background("yellow");
+    background("lightGrey");
+    let coreKeyword = new Keyword(0, canvasWidth/2, canvasHeight/2, "Core Keyword", "white");
+    coreKeyword.draw();
+    let someKeyword  = new Keyword(1, 150, 150, "some other Keyword", "lightBlue");
+    someKeyword.draw();
 }
