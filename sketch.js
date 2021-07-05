c032f31aaa1a81fdb2e36bf6549d6ca875ad6fab
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2021-07-05 11:09:29 +0200

    Now keywords are conected witt the line with the core word

--- sketch.js
+++ sketch.js
@@ -2,11 +2,11 @@ let canvas, canvasDiv, canvasWidth, canvasHeight, mindmap, input, newText;
 
 class MindMap {
     constructor() {
-        this.keywords = Array(0);
+        this.keywords = Array({x: 300, y: 200, w: 270, h: 30, text: "click to edit the Core Keyword", color: "pink"});
     }
     addKeyword(keyword) {
         const keywords = this.keywords.slice();
-        keywords.push(keyword);
+        keywords.unshift(keyword);
         this.keywords = keywords;
 
         console.log("new keyword was added:");
@@ -16,11 +16,24 @@ class MindMap {
     editKeyword(i, text) {
         const keywords = this.keywords.slice();
         keywords[i].text = text;
+        keywords[i].w = text.length*9;
         this.keywords = keywords;
     }
     draw() {
         for (let i = 0; i < this.keywords.length; i++) {
-            this.keywords[i].draw();
+            //this.keywords[i].draw();
+            let keyword = this.keywords[i];
+            //line
+            line(keyword.x, keyword.y, this.keywords[this.keywords.length - 1].x, this.keywords[this.keywords.length - 1].y);
+            // shape (currently rect with rounded corners)
+            fill(keyword.color);
+            rectMode(CENTER);
+            rect(keyword.x, keyword.y, keyword.w, keyword.h, 5);
+            // text
+            fill("black");
+            textAlign("center", "center");
+            textSize(15);
+            text(keyword.text, keyword.x, keyword.y);
         }
     }
     onClick(i) {
@@ -29,8 +42,7 @@ class MindMap {
         input = createInput("");
         input.position(10, 10);
         input.input(() => {
-            newText = input.value();
-                
+            newText = input.value(); 
         });
         let button = createButton("save new text");
         button.position(10 + input.width, 10);
@@ -47,11 +59,14 @@ class Keyword {
         this.text = text;
         this.x = x;
         this.y = y;
-        this.w = this.text.length*8; // width depends on text length
+        this.w = this.text.length*9; // width depends on text length
         this.h = 30;
         this.color = color;
     }
+    /*
     draw() {
+        //line
+        line(this.x, this.y, 300, 200);
         // shape (currently rect with rounded corners)
         fill(this.color);
         rectMode(CENTER);
@@ -62,6 +77,7 @@ class Keyword {
         textSize(15);
         text(this.text, this.x, this.y);
     }
+    */
 }
 
 function setup() {
@@ -76,10 +92,7 @@ function setup() {
     canvasDiv.style.height = canvasHeight + "px";
 
     mindmap = new MindMap();
-
-    let coreKeyword = new Keyword(canvasWidth/2, canvasHeight/2, "click to edit the Core Keyword", "pink");
-    mindmap.addKeyword(coreKeyword);
-
+    
     canvasDiv.addEventListener("click", (e) => {
         for (let i = 0; i < mindmap.keywords.length; i++) {
             if (mouseX >= mindmap.keywords[i].x - mindmap.keywords[i].w/2 &&
@@ -101,13 +114,12 @@ function mouseReleased(i) {
             mouseX <= mindmap.keywords[i].x + mindmap.keywords[i].w/2 &&
             mouseY >= mindmap.keywords[i].y - mindmap.keywords[i].h/2 &&
             mouseY <= mindmap.keywords[i].y + mindmap.keywords[i].h/2) {
-                mindmap.onClick(i);
-            }
+            mindmap.onClick(i);
+        }
     }
 }
 
 function draw() {
     background("lightGrey");
-    
     mindmap.draw();
 }
